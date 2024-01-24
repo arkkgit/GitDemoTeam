6f640183a573bc09f782a67555f60dd700a35142
arkkazure <abdul.r.k.k@outlook.com>
2024-01-24 11:57:34 +0000

    Add project files.

---
+++ GitDemoTeam/Team/Abdul.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemoTeam.Team
+{
+    internal class Abdul
+    {
+        //This is abdul Code.
+    }
+}

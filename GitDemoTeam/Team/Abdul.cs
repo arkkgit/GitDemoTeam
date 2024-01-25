c4f169d89d1e3bcaf0c7d3bf089a0bd24bcfe8b6
arkkazure <abdul.r.k.k@outlook.com>
2024-01-25 10:12:10 +0000

    new change of n1

--- GitDemoTeam/Team/Abdul.cs
+++ GitDemoTeam/Team/Abdul.cs
@@ -10,31 +10,6 @@ namespace GitDemoTeam.Team
     {
         //This is abdul Code.
         //Another line of code
-
-        //Login function by Chnadra
-        //login
-        //enterpassword and user name and clcik submit
-        //Login done
-
-        //login another user
-        // chnages from ab1 --- Changed
-        //abacd
-        //added
-
-        //ar2 f1
-
-        //ar2 f1
-
-        //ar3 f1
-
-        //ar3 -c1
-
-        //ar3 c2
-        //ar3 =c4
-        //b11
-        //b12
-        //b13
-        //m1
-        //m2
+        // this is from new branch
     }
 }
